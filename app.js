48292ac7c4be89815001381bac4ee9c82985b6c3
Dustin Eastes <dbeastes@gmail.com>
2016-08-16 14:30:06 -0500

    Consolidated routes and simplified app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+/* Primary Application interface
+
+Date                            notes       
+8-16-16                         Initial Create.
+
+*/
+
 var express = require('express');
 var path = require('path');
 var app = express();
 var name = "Dustin Eastes";
 var bodyParser = require('body-parser');
+var routes = require('./routes')
 
 app.use(express.static('routes'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,82 +21,10 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-  var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-        console.log('Database is accessed');
-      // we're connected!
-    });
+// use the routing logic here.
+app.use('/', routes);
+// connect to the mongoos database.
 
-var kittySchema = mongoose.Schema({
-    name: String,
-    weight: String
-});
-var cat = mongoose.model('cat', kittySchema);
-
-app.get('/', function(req,res){
-    var kittens;
-    
-    cat.find(function (err, cat) {
-        if (err) return console.error(err);
-        kittens = cat;
-        console.log(kittens);
-        
-        
-         res.render('home', {
-        message:    "Add Your Kittens Here!",
-        kittens: kittens,
-        oneLastThing: "",
-        layout:     "Dig in the on this!"
-    });
-    });
-
-   
-});
-app.post('/', function(req,res){
-    var message = 'New Cat Added!';
-    console.log('A Post on the home page');
-    console.log(req.body);
-    cat.findOne({name: req.body.name}, function(err, thor) {
-        if (err) return console.error(err);
-        console.log(req.body.name);
-        console.log(thor);
-        if(thor){
-            
-            message = 'That Kitten Already Exists.';
-            console.log('Changing message.');
-            console.log(message);
-            renderPage(res, message);
-            
-        }
-        else{
-            
-            renderPage(res, message);
-            var newKitten = new cat({
-                name:req.body.name,
-                weight:req.body.weight
-            });
-            newKitten.save();
-        }
-        
-    });
-});
-
-app.get('/kitten', function(req,res){
-    cat.find(function (err, cat) {
-        if (err) return console.error(err);
-        var kittens = cat;
-        console.log(kittens);
-        
-        res.render('kitten', {
-        message:'The Kittens!',
-        kittens:kittens
-        }); 
-    });
-});
-   
 
 
 function renderPage(res, message){
